c8a9a36ef117c34f72140d8f608980108a069645
varshabh <varshabh@uw.edu>
2025-04-02 18:31:51 -0700

    testing push

--- SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
+++ SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
@@ -27,7 +27,7 @@ print("Welcome to the UW Calculator Playground")
 //: For this latter set of operations, it is safe to assume that `["count"]` (with no additional arguments) is 0, `["avg"]` is also 0, and `["fact"]` is 0. `["1", "fact"]` should return 1, and `["0", "fact"]` should also return 1. (Yes, 0-factorial is 1. True story.)
 //: 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    return -10
 }
 
 func calculate(_ arg: String) -> Int {
